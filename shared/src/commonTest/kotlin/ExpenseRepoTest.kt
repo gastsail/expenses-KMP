17970aa6c571d69aa35df05dbe34da28376b548d
gastonsaillen <gsaillen95@gmail.com>
2023-08-23 14:41:39 -0300

    Added first tests

--- shared/src/commonTest/kotlin/ExpenseRepoTest.kt
+++ shared/src/commonTest/kotlin/ExpenseRepoTest.kt
@@ -1,15 +1,10 @@
 import data.fakeExpenseList
 import domain.ExpenseRepository
 import io.mockative.Mock
-import io.mockative.any
 import io.mockative.classOf
-import io.mockative.doesNothing
 import io.mockative.every
 import io.mockative.mock
-import model.Expense
-import model.ExpenseCategory
 import kotlin.test.Test
-import kotlin.test.assertNotEquals
 import kotlin.test.assertSame
 
 class ExpenseRepoTest {
